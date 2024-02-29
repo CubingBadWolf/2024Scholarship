a559e15c5deef2a3a21a5913e36b5021165db9ec
CubingBadWolf <Cbauchop13@gmail.com>
2024-02-29 15:02:19 +1300

    timetables folder creation

---
+++ timetables.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Timetable{
+    protected:
+        std::string Day;
+        int teacherID;
+    public:
+        std::vector<std::string> BuildTimetable(std::string day){
+            
+        }
+
+};
