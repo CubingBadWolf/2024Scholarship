6a350668a959f5c324ec38a51e6c38cbc458003a
CubingBadWolf <Cbauchop13@gmail.com>
2024-03-06 14:07:40 +1300

    Implimented Period and single day timetable. To Test.

--- timetables.cpp
+++ timetables.cpp
@@ -1,14 +1,55 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 
-class Timetable{
+
+class Period{
+public:
+    std::string Status;
+    char Status_Short; 
+    Period(char code){
+        Status_Short = code;
+        switch(code){
+            case 'P': Status = "Present";
+            case 'U': Status = "Absent Unassigned";
+            case 'F': Status = "Absent Assigned";
+            case 'W': Status = "Work attatched";
+            case 'X': Status = "Not Teaching";
+            default: Status = "Undefined";
+                //throw "Error";
+        }
+    }
+};
+
+class DayTimetable{
+    private:
+    int periodsInDay[5] = {5,5,5,6,5};
+
     protected:
-        std::string Day;
+        int Day;
         int teacherID;
     public:
-        std::vector<std::string> BuildTimetable(std::string day){
-            
+        std::vector<Period> DayTimetable;
+
+        void BuildDayTimetable(int day){
+            Day = day;
+            for(int i=0; i < periodsInDay[day]; i++){
+                DayTimetable.push_back(Period('P'));
+            }
         }
 
+        void PrintTimetable(){
+            if(DayTimetable.size()==1){
+                for(int item=0; item < periodsInDay[Day]; item++){
+                    std::cout << "Period " << item << ": " << DayTimetable[item].Status_Short << std::endl;
+                }
+            }
+        }
 };
+
+int main(){
+    DayTimetable Monday;
+    Monday.BuildDayTimetable(0);
+    Monday.PrintTimetable();
+}
