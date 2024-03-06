8f85b5cc070cf7f99e15b360267c37fe7238d231
CubingBadWolf <gaming1dragons@gmail.com>
2024-03-06 17:36:21 +1300

    Week timetable object added

--- timetables.cpp
+++ timetables.cpp
@@ -27,30 +27,57 @@ class DayTimetable{
     int periodsInDay[5] = {5,5,5,6,5};
 
     protected:
+        std::string Days[5] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
         int Day;
         int teacherID;
     public:
-        std::vector<Period> DayTimetable;
+        std::vector<Period> dayTimetable;
 
-        void BuildDayTimetable(int day){
+        DayTimetable(int day){
             Day = day;
-            for(int i=0; i < periodsInDay[day]; i++){
-                DayTimetable.push_back(Period('P'));
+            for(int i = 0; i < periodsInDay[day]; i++){
+                dayTimetable.push_back(Period('P'));
             }
         }
 
         void PrintTimetable(){
+            std::cout << Days[Day] << std::endl;
             for(int item=0; item < periodsInDay[Day]; item++){
-                std::cout << "Period " << item << ": " << DayTimetable[item].Status_Short << std::endl;
+                std::cout << "Period " << item << ": " << dayTimetable[item].Status_Short << std::endl;
             }
            
         }
 };
 
+class WeekTimetable{
+    private:
+    std::string Days[5] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+    public:
+    int WeekNumber;
+    std::vector<DayTimetable> weekTimetable; //A week timetable is just a collection of 5 day timetables if you think about it.
+
+    WeekTimetable(int weekNumber){
+        WeekNumber = weekNumber;
+        for(int i = 0; i < 5; i++){
+            weekTimetable.push_back(DayTimetable(i));
+        }
+    }
+    void PrintTimetable(){
+        std::cout << "Week " << WeekNumber << ":" << std::endl;
+        for(int i = 0; i < 5; i++){
+            weekTimetable[i].PrintTimetable();
+            std::cout << std::endl;
+        }
+    }
+};
+
 int main(){
-    DayTimetable Monday;
-    Monday.BuildDayTimetable(0);
+    DayTimetable Monday(0);
     Monday.PrintTimetable();
+
+    WeekTimetable WeekOne(1);
+    WeekOne.PrintTimetable();
+
     char temp;
     std::cin >> temp; //Temp process to keep window open. 
     return 0;
